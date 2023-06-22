efa7c2bb2f0d95cdd8343ec53edff6e676da10ad
k2hmr <k2hmr94@gmail.com>
2023-06-23 00:16:44 +0900

    refactor: routerファイルを削除して直接Controllerを代入

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
-import userRouter from "./infra/userRouter";
+import { UserController } from "./infra/controller/UserController";
+import { verifyToken } from "./infra/middleware/verifyToken";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(userRouter);
-
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("hello express\n");
 });
+
+const userController = new UserController();
+app.post("/register", verifyToken, userController.register);
+app.post("/login", verifyToken, userController.login);
+app.get("/users", userController.fetchAllUsers);
 
 export default app;
