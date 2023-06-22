6f467b632bbf82d4b63220c145fb3af4966a8c03
k2hmr <k2hmr94@gmail.com>
2023-06-22 20:18:45 +0900

    refactor: PrismaORMで取得したUserをUserEntityで変換する

--- src/infra/controller/UserController.ts
+++ src/infra/controller/UserController.ts
@@ -13,32 +13,17 @@ export class UserController {
   async register(req: Request, res: Response) {
     const { name, email, password } = req.body;
     const registerByEmailAndPasswordUseCase = new RegisterByEmailAndPasswordUseCase(this.userRepository);
-    try {
-      await registerByEmailAndPasswordUseCase.execute(name, email, password);
-      res.status(200).json({ message: "Register successful" });
-    } catch (error) {
-      throw unauthorizedException(error.message);
-    }
+    await registerByEmailAndPasswordUseCase.execute(name, email, password);
   }
 
   async login(req: Request, res: Response) {
     const { email, password } = req.body;
     const loginByEmailAndPasswordUseCase = new LoginByEmailAndPasswordUseCase(this.userRepository);
-    try {
-      await loginByEmailAndPasswordUseCase.execute(email, password);
-      res.status(200).json({ message: "Login successful" });
-    } catch (error) {
-      throw unauthorizedException(error.message);
-    }
+    await loginByEmailAndPasswordUseCase.execute(email, password);
   }
 
   async fetchAllUsers(req: Request, res: Response) {
     const fetchAllUsersUseCase = new FetchAllUsersUseCase(this.userRepository);
-    try {
-      const users = await fetchAllUsersUseCase.execute();
-      res.status(200).json({ users });
-    } catch (error) {
-      throw unauthorizedException(error.message);
-    }
+    const users = await fetchAllUsersUseCase.execute();
   }
 }
--- src/infra/repository/UserRepository.ts
+++ src/infra/repository/UserRepository.ts
@@ -1,30 +1,43 @@
 import { PrismaClient } from "@prisma/client";
 import { User } from "../../domain/user/UserEntity";
 import { IUserRepository } from "../../domain/user/IUserRepository";
+import { unauthorizedException } from "../../exception/error";
 
 const prisma = new PrismaClient();
 
 export class UserRepository implements IUserRepository {
   async create(name: string, email: string, password: string): Promise<void> {
-    // データベースなどにユーザー情報を保存する処理を実装
-    await prisma.user.upsert({
-      where: { email: email },
-      update: {},
-      create: {
-        name: name,
-        email: email,
-        password: password,
-      },
-    });
+    try {
+      await prisma.user.create({
+        data: {
+          name: name,
+          email: email,
+          password: password,
+        },
+      });
+    } catch (error) {
+      throw unauthorizedException(error.message);
+    }
   }
 
-  async find(email: string, password: string): Promise<User | null> {
-    return await prisma.user.findFirst({
-      where: { email: email, password: password },
-    });
+  async find(email: string, password: string): Promise<User> {
+    try {
+      const user = await prisma.user.findFirst({
+        where: { email: email, password: password },
+      });
+
+      const domainUser = new User(user.id, user.name, user.email, user.password, user.created_at);
+      return domainUser;
+    } catch (error) {
+      throw unauthorizedException(error.message);
+    }
   }
 
-  async findAll(): Promise<User[] | []> {
-    return await prisma.user.findMany();
+  async findAll(): Promise<User[]> {
+    const users = await prisma.user.findMany();
+    const domainUsers = users.map((user) => {
+      return new User(user.id, user.name, user.email, user.password, user.created_at);
+    });
+    return domainUsers;
   }
 }
