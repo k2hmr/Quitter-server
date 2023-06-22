6a937855a453343cf734aac476fda14cc65fc7e2
k2hmr <k2hmr94@gmail.com>
2023-06-23 00:17:28 +0900

    refactor: エラーのプロパティの設定を修正

--- src/exception/error.ts
+++ src/exception/error.ts
@@ -1,6 +1,6 @@
 export class HttpException extends Error {
-  statusCode?: number;
-  message: string;
+  public readonly statusCode?: number;
+  public readonly message: string;
   constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode || 500;
