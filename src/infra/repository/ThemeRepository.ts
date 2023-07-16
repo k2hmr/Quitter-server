f443cadcfc30556e9ffe101dc1591c5fae136a60
k2hmr <k2hmr94@gmail.com>
2023-07-16 21:51:11 +0900

    refactor: コンストラクタで型を変換するようにする

--- src/infra/repository/ThemeRepository.ts
+++ src/infra/repository/ThemeRepository.ts
@@ -2,43 +2,18 @@ import { PrismaClient } from "@prisma/client";
 import { IThemeRepository } from "../../domain/theme/IThemeRepository";
 import { Theme } from "../../domain/theme/ThemeEntity";
 import { internalErrorException, notFoundException, unprocessableEntityException } from "../../exception/error";
-import { PriorityType } from "../../domain/theme/priority";
-import { PlatformType } from "../../domain/theme/platform";
+import { IPriority, Priority, PriorityType } from "../../domain/theme/priority";
+import { IPlatform, Platform, PlatformType } from "../../domain/theme/platform";
 
 const prisma = new PrismaClient();
-const priorityName = (priorityIndex: number): PriorityType => {
-  switch (priorityIndex) {
-    case 1:
-      return PriorityType.Low;
-    case 2:
-      return PriorityType.Middle;
-    case 3:
-      return PriorityType.High;
-    default:
-      throw internalErrorException("優先度の値が誤っています。");
-  }
-};
-const platformName = (platformIndex: number): PlatformType => {
-  switch (platformIndex) {
-    case 1:
-      return PlatformType.Note;
-    case 2:
-      return PlatformType.Youtube;
-    case 3:
-      return PlatformType.Twitter;
-    case 4:
-      return PlatformType.Tiktok;
-    case 5:
-      return PlatformType.Qiita;
-    case 6:
-      return PlatformType.Zenn;
-    case 7:
-      return PlatformType.Other;
-    default:
-      throw internalErrorException("プラットフォームの値が誤っています。");
-  }
-};
+const convertToVOType = (priority: number, platform: number): { priority: PriorityType; platform: PlatformType } => {
+  const priorityDataVal: IPriority = { priority: priority as PriorityType };
+  const _priority = Priority.create(priorityDataVal).priority;
+  const platformDataVal: IPlatform = { platform: platform as PlatformType };
+  const _platform = Platform.create(platformDataVal).platform;
 
+  return { priority: _priority, platform: _platform };
+};
 export class ThemeRepository implements IThemeRepository {
   async create(
     theme: string,
@@ -61,12 +36,14 @@ export class ThemeRepository implements IThemeRepository {
         throw internalErrorException(error.message);
       });
 
+    const converted = convertToVOType(createdTheme.priority, createdTheme.platform);
+
     const domainTheme = new Theme(
       createdTheme.id,
       createdTheme.theme,
       createdTheme.categoryId,
-      priorityName(createdTheme.priority),
-      platformName(createdTheme.platform),
+      converted.priority,
+      converted.platform,
       createdTheme.createdAt,
       createdTheme.userId
     );
@@ -79,13 +56,14 @@ export class ThemeRepository implements IThemeRepository {
       const targetTheme = await prisma.theme.findFirst({
         where: { id: id, userId: userId },
       });
+      const converted = convertToVOType(targetTheme.priority, targetTheme.platform);
 
       const domainTheme = new Theme(
         targetTheme.id,
         targetTheme.theme,
         targetTheme.categoryId,
-        priorityName(targetTheme.priority),
-        platformName(targetTheme.platform),
+        converted.priority,
+        converted.platform,
         targetTheme.createdAt,
         targetTheme.userId
       );
@@ -102,12 +80,14 @@ export class ThemeRepository implements IThemeRepository {
   async findAll(userId: string): Promise<Theme[]> {
     const themes = await prisma.theme.findMany({ where: { userId: userId } });
     return themes.map((theme) => {
+      const converted = convertToVOType(theme.priority, theme.platform);
+
       return new Theme(
         theme.id,
         theme.theme,
         theme.categoryId,
-        priorityName(theme.priority),
-        platformName(theme.platform),
+        converted.priority,
+        converted.platform,
         theme.createdAt,
         theme.userId
       );
